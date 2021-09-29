14cc45b8f7b46902297f13d81a4edad30aed1ff0
hamzaCH1991 <hamza.chaabani@sinequa.com>
2021-09-29 11:52:43 +0200

    filter palette's widgets already displayed

--- projects/usage-analytics/src/app/audit/widget-palette/widget-palette.component.ts
+++ projects/usage-analytics/src/app/audit/widget-palette/widget-palette.component.ts
@@ -1,5 +1,7 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, HostListener, OnDestroy, OnInit } from "@angular/core";
+import { SearchService } from "@sinequa/components/search";
 import { AppService } from "@sinequa/core/app-utils";
+import { Subscription } from "rxjs";
 import { DashboardItemOption, DashboardService } from "../dashboard/dashboard.service";
 
 @Component({
@@ -7,19 +9,42 @@ import { DashboardItemOption, DashboardService } from "../dashboard/dashboard.se
     templateUrl: "./widget-palette.component.html",
     styleUrls: ["./widget-palette.component.scss"],
 })
-export class WidgetPaletteComponent implements OnInit {
+export class WidgetPaletteComponent implements OnInit, OnDestroy {
 
     showPalette = false;
     palette: {name: string, items: DashboardItemOption[]}[];
 
+    private _querySubscription: Subscription;
+    private _dashboardChangesSubscription: Subscription;
+
     constructor(
         public dashboardService: DashboardService,
+        public searchService: SearchService,
         public appService: AppService
     ) {
+        /** Update palette after each dashboard change */
+        this._dashboardChangesSubscription = this.dashboardService.dashboardChanged.subscribe(() => this.palette = this.getPalette());
+
+        /** Update palette after query change ( open new dashboard, shared dashboard ...) */
+        this._querySubscription = this.searchService.queryStream.subscribe(() => this.palette = this.getPalette());
     }
 
     ngOnInit() {
-        this.palette = this.dashboardService.getPalette();
+        this.palette = this.getPalette()
+    }
+
+    ngOnDestroy() {
+        this._querySubscription?.unsubscribe();
+        this._dashboardChangesSubscription?.unsubscribe();
+    }
+
+    getPalette(): {name: string, items: DashboardItemOption[]}[] {
+        return this.dashboardService.getPalette().map(
+            (p: {name: string, items: DashboardItemOption[]}) => ({
+                name: p.name,
+                items: p.items.filter((widget) => !this.dashboardService.dashboard.items.find((item) => item.title === widget.text))
+            })
+        )
     }
 
     togglePalette() {
